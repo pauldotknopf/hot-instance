1b25efcbc83d9bb5d8d705683eacd59a08d8ec40
Paul Knopf <pauldotknopf@gmail.com>
2016-07-19 18:08:11 -0400

    Minor updates.

--- index.js
+++ index.js
@@ -8,16 +8,28 @@ function LiveInstance(module) {
   this.moduleLocation = require.resolve(module);
 
   if(!fs.existsSync(this.moduleLocation)) {
-    this.error = 'The module ' + this.module + ' could not be located';
+    this.exists = false;
+    this.error = 'No module could be found for ' + module + '.';
     return;
+  } else {
+    this.instance = require(this.moduleLocation);
+    this.exists = true;
   }
 
-  // let's load the module now, and begin watching it for changes
-  this.instance = require(module);
-
-  this.watcher = chokidar.watch(this.moduleLocation).on('all', (event, path) => {
-    this.instance = reload(this.module);
-  });
+  // start watching the file
+  this.watcher = chokidar.watch(this.moduleLocation)
+    .on('add', (event, path) => {
+      this.instance = reload(this.moduleLocation);
+      this.exists = true;
+    })
+    .on('unlink', (event, path) => {
+      this.exists = false;
+      this.instance = null;
+    })
+    .on('change', (event, path) => {
+      this.instance = reload(this.moduleLocation);
+      this.exists = true;
+    });
 }
 
 module.exports = function(module) {
